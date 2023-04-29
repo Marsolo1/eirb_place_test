368d475309f8d4ea4e8a20bb23bb90b79b219721
Marsolo1 <martialmarzloff@gmail.com>
2023-04-30 04:03:55 +0900

    added template

---
+++ template.js
@@ -0,0 +1,4 @@
+const i = document.createElement("img");
+i.src = "https://cdn.discordapp.com/attachments/1100407387145371740/1101874914207744050/pixil-frame-0.png";
+i.style = "position: absolute;left:70.5px;top:269.5px;image-rendering: pixelated;width: 23px;height:30px;opacity:0.5;";
+document.getElementById("template-images").appendChild(i)
